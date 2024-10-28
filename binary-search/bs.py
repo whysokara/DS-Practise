22ffbef220e0355a2ab0c1522bc4adc70314c5f4
Kara <kara_himanshu@yahoo.com>
2024-10-29 00:34:40 +0530

    first commit

---
+++ binary-search/bs.py
@@ -0,0 +1,17 @@
+def binary_search(arr, target):
+    l,r = 0
+
+    while l <= r:
+        mid = (l+r) // 2
+
+        if arr[mid] = target:
+            return mid
+        elif arr[mid] < target:
+            r = mid - 1
+        else:
+            l = mid + 1
+    return -1
+
+arr = [2,5,7,9,12,14,15,46,79]
+target = 9
+binary_search(arr, target)
